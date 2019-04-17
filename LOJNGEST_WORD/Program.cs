276bf5078fc9deb002636a12b77e6950bf3c1117
visualstudio <visualstudio@122-PC27>
2019-04-17 10:23:56 -0400

    LONGEST WORD FINAL

--- LOJNGEST_WORD/Program.cs
+++ LOJNGEST_WORD/Program.cs
@@ -1,28 +1,46 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LOJNGEST_WORD
+{
+
+
     class Program
     {
         static void Main(string[] args)
         {
             Program a = new Program();
-            Console.WriteLine("the longest world is :");
-            Console.WriteLine(a.MyFunctionA("ada lovelace wrote the first algorith desingned for processing bt an Analytical Engine."));
+            Console.WriteLine("The Longest word is :");
+            Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first alogirithm designed for processing by an Analytical Engine."));
         }
-
-        public MyFunctionA(string input)
+        public string MyFunctionA(string input)
         {
-            // Write a C# prograam to find the longest word in a string.
-            string[] words = input.Split('');
-
+            string[] words = input.Split(' ');
             int wordArrayLength = words.Length;
-            int[] wordsLength = new int[wordArrayLength];
+            int[] wordLength = new int[wordArrayLength];
             int x = 0;
+            string aa = " ";
+            foreach (var word in words)
+            {
+                if (word.Length > x)
+                {
+                    input = word;
+                    aa = input;
+                    x = word.Length;
+                }
+            }
+            string currentWord = words[0];
+            string nextWord;
+            string longestWord = currentWord;
+            for (int y = 0; y < words.Length - 1; y++)
+            {
+                currentWord = words[y];
+                nextWord = words[y + 1];
+            }
+            return aa;
         }
     }
 }
