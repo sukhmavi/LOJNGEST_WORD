e61052a7b90a6b04c3424b3d44ab9f290d41b9c9
visualstudio <visualstudio@122-PC27>
2019-04-17 10:19:22 -0400

    LONGEST WORD

--- LOJNGEST_WORD/Program.cs
+++ LOJNGEST_WORD/Program.cs
@@ -1,15 +1,28 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LOJNGEST_WORD
-{
     class Program
     {
         static void Main(string[] args)
         {
+            Program a = new Program();
+            Console.WriteLine("the longest world is :");
+            Console.WriteLine(a.MyFunctionA("ada lovelace wrote the first algorith desingned for processing bt an Analytical Engine."));
+        }
+
+        public MyFunctionA(string input)
+        {
+            // Write a C# prograam to find the longest word in a string.
+            string[] words = input.Split('');
+
+            int wordArrayLength = words.Length;
+            int[] wordsLength = new int[wordArrayLength];
+            int x = 0;
         }
     }
 }
